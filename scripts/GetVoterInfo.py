7b7f87aa41cf7414262843a987e15b9161528bf8
fftestusr <74042482+fftestusr@users.noreply.github.com>
2020-11-07 19:01:38 -0800

    Update GetVoterInfo.py

--- scripts/GetVoterInfo.py
+++ scripts/GetVoterInfo.py
@@ -3,8 +3,8 @@ import csv
 import json
 import grequests
 
-inputFile = "..\\detroit_index.txt"
-outputFile = "..\\detroit_outputfile_py.txt"
+inputFile = "C:\\Users\\kfang\\Downloads\\detroit_index.txt"
+outputFile = "C:\\Users\\kfang\\Downloads\\detroit_outputfile_py.txt"
 getInfoUri = "https://mvic.sos.state.mi.us/Voter/SearchByName"
 
 monthArray = ["1","2","3","4","5","6","7","8","9","10","11","12"]
@@ -32,14 +32,19 @@ ballotRetDateRegExp = re.compile('<b>Ballot\s+received<\/b><br\s*\/>(\d{1,2}\/\d
 
 ballotRet_list = []
 
+i = 0
 for rst in res_list:
-    ballotRetDateInfo = ballotRetDateRegExp.search(rst.text)
-    if ballotRetDateInfo:
-        tmpDict = dict((x.strip(), y.strip())
-                    for x, y in (element.split('=')
-                    for element in rst.request.body.split('&')))
-        tmpDict["BallotRetDate"] = ballotRetDateInfo.group(1)
-        ballotRet_list.append(tmpDict)
+    i = i + 1
+    if rst is not None:
+        ballotRetDateInfo = ballotRetDateRegExp.search(rst.text)
+        if ballotRetDateInfo:
+            tmpDict = dict((x.strip(), y.strip())
+                        for x, y in (element.split('=')
+                        for element in rst.request.body.split('&')))
+            tmpDict["BallotRetDate"] = ballotRetDateInfo.group(1)
+            ballotRet_list.append(tmpDict)
+    else:
+        print("No Result.")
 
 with open(outputFile, 'w') as outfile:
     json.dump(ballotRet_list, outfile)
